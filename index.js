12abb0fa6ae69685d78c358bd51fed1022c188fa
charcOOal <sarka-stranska@seznam.cz>
2025-06-08 20:28:52 +0200

    feat: display searched books on the homepage

--- index.js
+++ index.js
@@ -16,67 +16,59 @@ app.get("/", (req, res) => {
 
 const apiURL = "https://www.googleapis.com/books/v1/volumes?q=intitle:"
 
-
-/* function findGoogleBooks(allFoundBooks) {
-
-    let searchedBooks = [];
-
-    allFoundBooks.forEach(foundBook => {
-        const book = foundBook.volumeInfo;
-        const title = book.title;
-        const author = book.authors; 
-        const date = book.publishedDate;
-        const isbnObj = book.industryIdentifiers;
-        let isbn;
-        if (typeof isbnObj === 'object') {
-            const isbn13 = isbnObj.find(({type}) => type == 'ISBN_13');
-            if (isbn13) {
-                isbn = isbn13.identifier;
-            } else {
-                console.log("NOT FOUND");
-            }
-        }
-        console.log(title, author);
-        if (title && author && date && isbn) {
-            searchedBooks.push(title, author);
-        }
-    });
-    console.log(searchedBooks);
-} */
-
+// filter out non-desired books from google books api after search
 function filterGoogleBooks(allFoundBooks) {
     const filteredBooks = allFoundBooks.filter((foundBook) => {
-        
+        // a book has to have: title, author(s), date of publishing, ISBN 13 and cover image
         const book = foundBook.volumeInfo;
         const hasAllData = book
             && book.title
             && Array.isArray(book.authors) 
             && book.publishedDate
-            && Array.isArray(book.industryIdentifiers);
+            && Array.isArray(book.industryIdentifiers)
+            && book.imageLinks;
 
         const hasIsbn13 = hasAllData && book.industryIdentifiers.some(({type}) => type == 'ISBN_13');
 
-        /* const isbnObject = foundBook.volumeInfo.industryIdentifiers;
-        isbnObject.some(({type}) => type == 'ISBN_13'); */
         return hasAllData && hasIsbn13;
     })
-    filteredBooks.forEach(book => {
-        console.log("FILTER", book.volumeInfo.title, book.volumeInfo.authors, book.industryIdentifiers)
-    });
-
-    allFoundBooks.forEach(book => {
-        console.log(book.volumeInfo.title, book.volumeInfo.authors, book.industryIdentifiers)
-    });
+    return filteredBooks
 }
 
+// create simplified objects of filtered google books to display
+function displayGoogleBooks(cleanBooks) {
+    let displayedBooks = [];
+
+    cleanBooks.forEach(cleanBook => {
+        const bookIsbn = cleanBook.volumeInfo.industryIdentifiers.find(({type}) => type == 'ISBN_13')
+        const book = {
+            title: cleanBook.volumeInfo.title,
+            authors: cleanBook.volumeInfo.authors,
+            isbn: bookIsbn.identifier,
+            cover: cleanBook.volumeInfo.imageLinks.thumbnail
+        }
+        displayedBooks.push(book);
+    });
+    console.log(displayedBooks);
+    return displayedBooks
+}
+
+// using intitle only for now 
 app.post("/submit", async (req, res) => {
     try {
         const response = await axios.get(apiURL + encodeURIComponent(req.body.input));
-        // findGoogleBooks(response.data.items)
-        filterGoogleBooks(response.data.items)
+        const cleanBooks = filterGoogleBooks(response.data.items);
+        const searchBooks = displayGoogleBooks(cleanBooks);
+        res.render("index.ejs", {searchBooks: searchBooks})
+
     } catch (error) {
         console.error(error);
     }
+})
+
+// create new book based on search
+app.get("/new-book/:isbn", (req, res) => {
+    console.log("HELLO");
     res.redirect("/");
 })
 
