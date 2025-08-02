4430c0b92908043c20d05fccf0028357f87a3bdd
charcOOal <sarka-stranska@seznam.cz>
2025-08-02 09:22:40 +0200

    save version 1

--- index.js
+++ index.js
@@ -9,25 +9,16 @@ const port = 3000
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-/* const db = new pg.Client({
+// connect to database
+const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "bookshelf",
   password: "gandalf",
   port: 5432,
-}); */
-
-const db = new pg.Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
 });
 
-
 db.connect();
-
-let bookshelf = [];
 
 // homepage
 app.get("/", async (req, res) => {
